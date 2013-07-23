a55e065c3078c88b6b70626770a660e0e5de5a91
digital labs <success@dpsw.info>
2013-07-23 02:31:11 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 require('./timeago.js')();
 
 console.log(
+    new Date(1), _timeago(new Date(1))==='44 years ago'
+);
+console.log(
+    'today is ' + _today();
+);
+console.log(
     _timefriendly('1 hour'), _timefriendly('1 hour')===1000*60*60,
     _timefriendly('2 days'), _timefriendly('2 days')===1000*60*60*48,
     _timefriendly('2 weeks'), _timefriendly('2 weeks')===1000*60*60*24*14
