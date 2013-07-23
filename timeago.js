5edb4150742d290a312f287522b85e7c8cb4a512
digital labs <success@dpsw.info>
2013-07-23 02:29:06 -0500

    Update timeago.js

--- timeago.js
+++ timeago.js
@@ -1,24 +1,16 @@
 module.exports = function(){
     
-    var second = 1000, 
-        minute = second*60, 
-        hour = minute*60, 
-        day = hour*24, 
-        week = day*7, 
-        month = day*30, 
-        year = day*365;
+    var o = { second: 1000, minute: 60*1000, hour: 60*1000*60, day: 24*60*1000*60, week: 7*24*60*1000*60,
+			month: 30*24*60*1000*60, year: 365*24*60*1000*60 };
 
     global._timeago = function(nd){
-        var r = Math.round, pl = function(v, n){ return n + ' ' + v + (n>1 ? 's':'') + ' ago'};
-        var ts = new Date().getTime() - new Date(nd).getTime();
-        if(ts < second) return ts + ' ms ago';
-        if(ts > second && ts < minute) return pl('second', r(ts/second));
-        if(ts > minute && ts < hour) return pl('minute', r(ts/minute));
-        if(ts > hour && ts < day) return pl('hour', r(ts/hour));
-        if(ts > day && ts < week) return pl('day', r(ts/day));
-        if(ts > week && ts < month) return pl('week', r(ts/week));
-        if(ts > month && ts < year) return pl('month', r(ts/month));
-        return pl('year', r(ts/year));
+       	var r = Math.round, pl = function(v, n){ return n + ' ' + v + (n>1 ? 's':'') + ' ago'},
+       		ts = new Date().getTime() - new Date(nd).getTime(), ii;
+    	for(i in o){
+    		if(r(ts) <= o[i]) return pl(ii||'m', r(ts/(o[ii]||1)))
+    		ii = i;
+    	}
+    	return pl(i, r(ts/o[i]));
     }
     
     global._today = function(){
@@ -30,6 +22,6 @@ module.exports = function(){
 
     global._timefriendly = function(s){
         var t = s.match(/(\d).([a-z]*?)s?$/);
-        return t[1] * eval(t[2]);
+        return t[1] * eval(o[t[2]]);
     }
 }
