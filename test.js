51bdf496fef41225ea77cb316a5a96d5a66241dd
C Borkert <success@dpsw.info>
2013-08-21 13:58:58 -0500

    fix

--- test.js
+++ test.js
@@ -1,15 +1,29 @@
-require('./timeago.js')();
+
+var ta = (typeof module !== 'undefined' && module.exports) ?
+		require('./timeago.js')
+		:
+		timeago();
 
 console.log(
-	_timeago(new Date()-1000), _timeago(new Date()-1000)==='1 second ago',
-	_timeago(new Date()-60000*180), _timeago(new Date()-60000*180)==='3 hours ago',
-    new Date(1), _timeago(new Date(1))==='44 years ago'
+	ta.ago(new Date()-1000), _timeago(new Date()-1000)==='1 second ago',
+	ta.ago(new Date()-60000*180), _timeago(new Date()-60000*180)==='3 hours ago',
+    new Date(1), ta.ago(new Date(1))==='44 years ago'
 );
+
 console.log(
-    'Today is ' + _today()
+    'Today is ' + ta.today()
 );
+
 console.log(
-    _timefriendly('1 hour'), _timefriendly('1 hour')===1000*60*60,
-    _timefriendly('2 days'), _timefriendly('2 days')===1000*60*60*48,
-    _timefriendly('2 weeks'), _timefriendly('2 weeks')===1000*60*60*24*14
+    ta.timefriendly('1 hour'), _timefriendly('1 hour')===1000*60*60,
+    ta.timefriendly('2 days'), _timefriendly('2 days')===1000*60*60*48,
+    ta.timefriendly('2 weeks'), _timefriendly('2 weeks')===1000*60*60*24*14
+);
+
+console.log(
+	(function(){
+		var i = 0, text = '';
+    	while(i++ < 600) text += 'text ');
+		return ta.mintoread(text);
+	})();
 );
--- timeago.js
+++ timeago.js
@@ -1,9 +1,10 @@
-module.exports = function(){
+var timeago = function(){
     
     var o = { second: 1000, minute: 60*1000, hour: 60*1000*60, day: 24*60*1000*60, week: 7*24*60*1000*60,
 			month: 30*24*60*1000*60, year: 365*24*60*1000*60 };
+    var obj = {};
 
-    global._timeago = function(nd){
+    obj.ago = function(nd){
        	var r = Math.round, pl = function(v, n){ return n + ' ' + v + (n>1 ? 's':'') + ' ago'},
        		ts = new Date().getTime() - new Date(nd).getTime(), ii;
     	for(i in o){
@@ -13,15 +14,24 @@ module.exports = function(){
     	return pl(i, r(ts/o[i]));
     }
     
-    global._today = function(){
+    obj.today = function(){
         var now = new Date();
         var Weekday = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
         var Month = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
         return Weekday[now.getDay()]+", "+Month[now.getMonth()]+" "+now.getDate()+", "+now.getFullYear();
     }
 
-    global._timefriendly = function(s){
+    obj.timefriendly = function(s){
         var t = s.match(/(\d).([a-z]*?)s?$/);
         return t[1] * eval(o[t[2]]);
     }
+
+    obj.mintoread = function(text, altcmt, wpm){
+        var m = Math.round(text.split(' ').length/(wpm||200)); 
+        return (m || '< 1') + (altcmt||' min to read');
+    }
+    
+    return obj;
 }
+
+if(typeof module !== 'undefined' && module.exports) module.exports = timeago;
