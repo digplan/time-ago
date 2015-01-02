6e925d1a37e8fe0bbe4fd83f8581de02e84d1fa7
C Borkert <success@dpsw.info>
2015-01-02 11:27:42 -0600

    fix

--- test.js
+++ test.js
@@ -1,24 +1,24 @@
-
 var ta = (typeof module !== 'undefined' && module.exports) ?
-		require('./timeago.js')() :	timeago();
+  require('./timeago.js')() : timeago();
 
 
 console.log(
-	ta.ago(new Date()-1000), ta.ago(new Date()-1000)==='1 second ago',
-	ta.ago(new Date()-60000*180), ta.ago(new Date()-60000*180)==='3 hours ago',
-    new Date(1), ta.ago(new Date(1)), ta.ago(new Date(1))==='45 years ago'
+  ta.ago(new Date() - 1000), ta.ago(new Date() - 1000) === '1 second ago',
+  ta.ago(new Date() - 60000 * 180), ta.ago(new Date() - 60000 * 180) === '3 hours ago',
+  new Date(1), ta.ago(new Date(1)), ta.ago(new Date(1)) === '45 years ago'
 );
 
 console.log(
-    'Today is ' + ta.today()
+  'Today is ' + ta.today()
 );
 
 console.log(
-    ta.timefriendly('1 hour'), ta.timefriendly('1 hour')===1000*60*60,
-    ta.timefriendly('2 days'), ta.timefriendly('2 days')===1000*60*60*48,
-    ta.timefriendly('2 weeks'), ta.timefriendly('2 weeks')===1000*60*60*24*14
+  ta.timefriendly('1 hour'), ta.timefriendly('1 hour') === 1000 * 60 * 60,
+  ta.timefriendly('2 days'), ta.timefriendly('2 days') === 1000 * 60 * 60 * 48,
+  ta.timefriendly('2 weeks'), ta.timefriendly('2 weeks') === 1000 * 60 * 60 * 24 * 14
 );
 
-var i = 0, text = '';
-while(i++ < 600) text += 'text ';
+var i = 0,
+  text = '';
+while (i++ < 600) text += 'text ';
 console.log(text.length, 'chars, min to read', ta.mintoread(text));
--- timeago.js
+++ timeago.js
@@ -1,49 +1,49 @@
 var timeago = function() {
 
-    var o = {
-      second: 1000,
-      minute: 60 * 1000,
-      hour: 60 * 1000 * 60,
-      day: 24 * 60 * 1000 * 60,
-      week: 7 * 24 * 60 * 1000 * 60,
-      month: 30 * 24 * 60 * 1000 * 60,
-      year: 365 * 24 * 60 * 1000 * 60
-    };
-    var obj = {};
+  var o = {
+    second: 1000,
+    minute: 60 * 1000,
+    hour: 60 * 1000 * 60,
+    day: 24 * 60 * 1000 * 60,
+    week: 7 * 24 * 60 * 1000 * 60,
+    month: 30 * 24 * 60 * 1000 * 60,
+    year: 365 * 24 * 60 * 1000 * 60
+  };
+  var obj = {};
 
-    obj.ago = function(nd) {
-      var r = Math.round,
-        pl = function(v, n) {
-          return n + ' ' + v + (n > 1 ? 's' : '') + ' ago'
-        },
-        ts = new Date().getTime() - new Date(nd).getTime(),
-        ii;
-      for (i in o) {
-        if (r(ts) < o[i]) return pl(ii || 'm', r(ts / (o[ii] || 1)))
-        ii = i;
-      }
-      return pl(i, r(ts / o[i]));
+  obj.ago = function(nd) {
+    var r = Math.round,
+      pl = function(v, n) {
+        return n + ' ' + v + (n > 1 ? 's' : '') + ' ago'
+      },
+      ts = new Date().getTime() - new Date(nd).getTime(),
+      ii;
+    for (i in o) {
+      if (r(ts) < o[i]) return pl(ii || 'm', r(ts / (o[ii] || 1)))
+      ii = i;
     }
-
-    obj.today = function() {
-      var now = new Date();
-      var Weekday = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
-      var Month = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-      return Weekday[now.getDay()] + ", " + Month[now.getMonth()] + " " + now.getDate() + ", " + now.getFullYear();
-    }
-
-    obj.timefriendly = function(s) {
-      var t = s.match(/(\d).([a-z]*?)s?$/);
-      return t[1] * eval(o[t[2]]);
-    }
-
-    obj.mintoread = function(text, altcmt, wpm) {
-      var m = Math.round(text.split(' ').length / (wpm || 200));
-      return (m || '< 1') + (altcmt || ' min to read');
-    }
-
-    return obj;
+    return pl(i, r(ts / o[i]));
   }
+
+  obj.today = function() {
+    var now = new Date();
+    var Weekday = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+    var Month = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+    return Weekday[now.getDay()] + ", " + Month[now.getMonth()] + " " + now.getDate() + ", " + now.getFullYear();
+  }
+
+  obj.timefriendly = function(s) {
+    var t = s.match(/(\d).([a-z]*?)s?$/);
+    return t[1] * eval(o[t[2]]);
+  }
+
+  obj.mintoread = function(text, altcmt, wpm) {
+    var m = Math.round(text.split(' ').length / (wpm || 200));
+    return (m || '< 1') + (altcmt || ' min to read');
+  }
+
+  return obj;
+}
 
 if (typeof module !== 'undefined' && module.exports)
   module.exports = timeago;
